2bc8141824b26d86d30c689f4f2bf720adb07319
4jeR <bdlugosz98@gmail.com>
2020-03-04 19:20:14 +0100

    Moved classes to separate files

---
+++ proj-1/edge.py
@@ -0,0 +1,18 @@
+
+import tkinter as tk
+from tkinter import W
+
+class Edge:
+    count = 0
+    def __init__(self,index, node1, node2, arrow = False):
+        self.index = index
+        self.node1 = node1
+        self.node2 = node2
+        self.arrow = arrow
+        Edge.count += 1
+
+    def Draw(self, canvas):
+        if self.arrow:
+            canvas.create_line(self.node1.x, self.node1.y,self.node2.x, self.node2.y, arrow=tk.LAST)
+        else:
+            canvas.create_line(self.node1.x, self.node1.y,self.node2.x, self.node2.y)
---
+++ proj-1/node.py
@@ -0,0 +1,29 @@
+import tkinter as tk
+from tkinter import W
+
+class Node:
+    count = 0
+    def __init__(self, index, x = 0, y = 0, r = 35, neighbours = []):
+        self.index = index
+        self.x = x
+        self.y = y 
+        self.r = r
+        self.neighbours = [nb for nb in neighbours]
+        Node.count += 1
+        
+
+
+    def PrintNeighbours(self):
+        print("{}: {}".format(self.index, [n for n in self.neighbours]))
+
+    def Move(self, dx, dy):
+        self.x += dx
+        self.y += dy
+
+    def Draw(self, canvas): #center coordinates, radius
+        x0 = self.x - self.r
+        y0 = self.y - self.r
+        x1 = self.x + self.r
+        y1 = self.y + self.r
+        canvas.create_oval(x0, y0, x1, y1)
+        canvas.create_text(self.x-5, self.y, anchor=W, font="Arial",text="{}".format(self.index))
