bb8d745d25e81323bee16736040fd51c6d58236e
klaudia <klaudia.fil@gmail.com>
2020-04-05 12:36:24 +0200

    new name

--- main.py
+++ main.py
@@ -1,14 +1,15 @@
 from graph import *
 
+
 def main():
     root = tk.Tk()
-    root.title("Grafy - projekt 1")
+    root.title("Grafy - projekt WARTOWNICY")
     canvas = tk.Canvas(root, width=1200, height=900)
     canvas.pack()
 
     v = Graph(canvas)
-    
-    # v.IM_to_NM("examples/incidence_matrix.txt")     
+
+    # v.IM_to_NM("examples/incidence_matrix.txt")
     # v.IM_to_NL("examples/incidence_matrix.txt")
     # v.NL_to_NM("examples/neighbour_list.txt")
     # v.NL_to_IM("examples/neighbour_list.txt")
@@ -16,10 +17,9 @@ def main():
     # v.NM_to_NL("examples/neighbour_matrix.txt")
     v.Draw()
 
-    
     root.update()
     root.mainloop()
-    
+
+
 if __name__ == '__main__':
     main()
-    
