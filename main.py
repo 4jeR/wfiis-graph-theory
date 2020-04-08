43cc9ae8b74b88d8921a982ad79176604589e537
4jeR <bdlugosz98@gmail.com>
2020-04-08 13:06:23 +0200

    clean main.py

--- main.py
+++ main.py
@@ -7,9 +7,6 @@ def main():
     root = Tk()
     gui = GUI(root)
     
-    g = Graph()
-    g.FillGraphFromLogicSequence("logic_seq.txt", gui.canvas,1, True)
-    gui.Draw(g)
 
     root.update()
     root.mainloop()
