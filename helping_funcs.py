45b68ef4af7af555e0c74c74df17861072f89946
Baczyn <rac28@op.pl>
2020-04-16 11:26:20 +0200

    Add CanEdgeRandomize and AreUniqueInt

--- helping_funcs.py
+++ helping_funcs.py
@@ -27,6 +27,12 @@ def AreUnique(Samples):
            (Samples[0].node2 != Samples[1].node1) and
            (Samples[0].node2 != Samples[1].node2))
 
+def AreUniqueInt(a,b,c,d):
+    return((a != c) and
+           (a != d) and
+           (b != c) and
+           (b != d))
+
 
 def RandomizeIndex(a, b, bad_idx_list, seq):
     ok_idx = random.randint(a, b)
@@ -41,3 +47,28 @@ def NodeFromIndex(graph, idx):
             return n
         else:
             continue
+
+def CanEdegRandomize(graph):
+    if len(graph.nodes) < 4:
+        return False
+    else:
+        #if counter >=2 grpah can be randomized
+        counter  = 0
+        #variables for begin/end of the missing edge 
+        node1_idx = 0
+        node2_idx = 0
+        #for each nodes in graph
+        for i in range(len(graph.nodes)):
+        #for each possible neighbour
+            for j in range(len(graph.nodes)):
+                if( ((j+1) not in graph.nodes[i].neighbours) and ((j+1)!=graph.nodes[i].index)):
+                    if counter==0:
+                        counter+=1
+                        node1_idx = graph.nodes[i].index
+                        node2_idx = j+1
+                        break
+                    if counter==1 and AreUniqueInt(node1_idx,node2_idx,j+1,graph.nodes[i].index):
+                        counter+=1
+                        return True
+
+        return False
